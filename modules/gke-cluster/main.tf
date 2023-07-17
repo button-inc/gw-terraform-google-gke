27a07dba3b6ec36d4d4fb4b0ff3fcf3344f12959
Josh Gamache <joshua@button.is>
2023-07-17 07:51:29 -0600

    chore!: remove depricated attribute - see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster#workload_identity_config for info

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -10,12 +10,6 @@ terraform {
   required_version = ">= 1.4.6"
 }
 
-locals {
-  workload_identity_config = !var.enable_workload_identity ? [] : var.identity_namespace == null ? [{
-    identity_namespace = "${var.project}.svc.id.goog" }] : [{ identity_namespace = var.identity_namespace
-  }]
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # Create the GKE Cluster
 # We want to make a cluster with no node pools, and manage them all with the fine-grained google_container_node_pool resource
@@ -151,14 +145,6 @@ resource "google_container_cluster" "cluster" {
     content {
       state    = "ENCRYPTED"
       key_name = database_encryption.value
-    }
-  }
-
-  dynamic "workload_identity_config" {
-    for_each = local.workload_identity_config
-
-    content {
-      identity_namespace = workload_identity_config.value.identity_namespace
     }
   }
 
